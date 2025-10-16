1711a25bfd18743480a4e0f773f71105f94282e2
americogabriel <gabrieldouble007@gmail.com>
2025-10-16 18:47:45 -0300

    Função de registrar novas pessoas no model

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home,listarusers
 
 
 urlpatterns = [
     path('',home, name='url_home'),
+    path('usuarios/',listarusers, name='url_users'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .forms import UsuarioForm
+from .models import Usuarios
 
 def home(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        idade = request.POST.get('idade')
+
+        Usuarios.objects.create(nome = nome, idade = idade)
+        return redirect('url_users')
     data = {}
     data['form'] = UsuarioForm
     return render(request,'app/home.html',data)
+
+def listarusers(request):
+    data = {}
+    data['todos_usuarios'] = Usuarios.objects.all()
+    return render(request,'app/listar.html',data)
 
 
